a42957d786ab16b11fceecf98a84a366f69bc5d2
Masaki Yoshida <yoshida@kailash-tech.jp>
2020-04-27 15:33:54 +0900

    operatorsAliases追加

--- sequelize_container.js
+++ sequelize_container.js
@@ -1,5 +1,44 @@
 const Sequelize = (process.env.SEQUELIZE5 ? require('sequelize5') : require('sequelize4'));
 
+// TODO: operatorsAliasesを削除
+const Op = Sequelize.Op;
+const operatorsAliases = {
+  $eq: Op.eq,
+  $ne: Op.ne,
+  $gte: Op.gte,
+  $gt: Op.gt,
+  $lte: Op.lte,
+  $lt: Op.lt,
+  $not: Op.not,
+  $in: Op.in,
+  $notIn: Op.notIn,
+  $is: Op.is,
+  $like: Op.like,
+  $notLike: Op.notLike,
+  $iLike: Op.iLike,
+  $notILike: Op.notILike,
+  $regexp: Op.regexp,
+  $notRegexp: Op.notRegexp,
+  $iRegexp: Op.iRegexp,
+  $notIRegexp: Op.notIRegexp,
+  $between: Op.between,
+  $notBetween: Op.notBetween,
+  $overlap: Op.overlap,
+  $contains: Op.contains,
+  $contained: Op.contained,
+  $adjacent: Op.adjacent,
+  $strictLeft: Op.strictLeft,
+  $strictRight: Op.strictRight,
+  $noExtendRight: Op.noExtendRight,
+  $noExtendLeft: Op.noExtendLeft,
+  $and: Op.and,
+  $or: Op.or,
+  $any: Op.any,
+  $all: Op.all,
+  $values: Op.values,
+  $col: Op.col
+};
+
 const container = {};
 
 class SequelizeContainer {
@@ -35,6 +74,9 @@ class SequelizeContainer {
         decimalNumbers: true,
       },
     };
+    if (process.env.SEQUELIZE5) {
+      options.operatorsAliases = operatorsAliases;
+    }
 
     delete options.dialectOptions.collate;
 
